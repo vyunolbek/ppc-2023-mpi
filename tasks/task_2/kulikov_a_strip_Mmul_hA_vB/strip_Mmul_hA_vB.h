4be2df5acbd89f356436f89b16e5206d18621ce7
AveZorgen <78862872+AveZorgen@users.noreply.github.com>
2023-10-27 04:23:59 +0300

    done

--- tasks/task_2/kulikov_a_strip_Mmul_hA_vB/strip_Mmul_hA_vB.h
+++ tasks/task_2/kulikov_a_strip_Mmul_hA_vB/strip_Mmul_hA_vB.h
@@ -2,4 +2,10 @@
 #ifndef TASKS_TASK_2_KULIKOV_A_STRIP_MMUL_HA_VB_STRIP_MMUL_HA_VB_H_
 #define TASKS_TASK_2_KULIKOV_A_STRIP_MMUL_HA_VB_STRIP_MMUL_HA_VB_H_
 
+#include <boost/serialization/vector.hpp>
+
+std::vector<int> getRandomMatrix(int n);
+std::vector<int> getParallelMatrMul(std::vector<int> A, std::vector<int> B, int n);
+std::vector<int> getSequentialMatrMul(std::vector<int> A, std::vector<int> B, int n);
+
 #endif  // TASKS_TASK_2_KULIKOV_A_STRIP_MMUL_HA_VB_STRIP_MMUL_HA_VB_H_
