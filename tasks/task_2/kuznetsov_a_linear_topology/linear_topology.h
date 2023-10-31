e3ff3dab3016be12543b350244e5d3abebb301b2
Artyom <artyomka_working@mail.ru>
2023-10-31 16:53:07 +0300

    add task

--- tasks/task_2/kuznetsov_a_linear_topology/linear_topology.h
+++ tasks/task_2/kuznetsov_a_linear_topology/linear_topology.h
@@ -4,6 +4,10 @@
 
 #include <mpi.h>
 
-int sum(int a, int b);
+int get_next(int current, bool router);
+int get_prev(int current, bool router);
+bool in_route(int current, int src, int dest, bool router);
+void send_data_linear(void* data, int count, MPI_Datatype datatype, int src,
+                    int dest, int tag, MPI_Comm comm);
 
 #endif  // TASKS_TASK_2_KUZNETSOV_A_LINEAR_TOPOLOGY_LINEAR_TOPOLOGY_H_
--- tasks/task_2/kuznetsov_a_linear_topology/main.cpp
+++ tasks/task_2/kuznetsov_a_linear_topology/main.cpp
@@ -6,40 +6,108 @@
 
 #include "./linear_topology.h"
 
-TEST(MPI_TESTS, Test_1) {
+TEST(MPI_TESTS, Test_inc) {
   int rank = 0;
+  int size_world = 0;
+
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  ASSERT_EQ(1, 1);
+  MPI_Comm_size(MPI_COMM_WORLD, &size_world);
+
+  const int sender = 0;
+  const int receiver = size_world - 1;
+  int data = 0;
+
+  if (rank == sender) data = 500;
+
+  send_data_linear(&data, 1, MPI_INT, sender, receiver, 0, MPI_COMM_WORLD);
+
+  if (rank == receiver) {
+    ASSERT_EQ(500, data);
+  }
 }
 
-TEST(MPI_TESTS, Test_2) {
+TEST(MPI_TESTS, Test_dec) {
   int rank = 0;
+  int size_world = 0;
+
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  ASSERT_EQ(1, 1);
+  MPI_Comm_size(MPI_COMM_WORLD, &size_world);
+
+  const int sender = size_world - 1;
+  const int receiver = 0;
+  int data = 0;
+
+  if (rank == sender) data = 500;
+
+  send_data_linear(&data, 1, MPI_INT, sender, receiver, 0, MPI_COMM_WORLD);
+
+  if (rank == receiver) {
+    ASSERT_EQ(500, data);
+  }
 }
 
-TEST(MPI_TESTS, Test_3) {
+TEST(MPI_TESTS, Test_two_processors) {
   int rank = 0;
+  int size_world = 0;
+
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  ASSERT_EQ(1, 1);
+  MPI_Comm_size(MPI_COMM_WORLD, &size_world);
+
+  const int sender = 0;
+  const int receiver = 1;
+  int data = 0;
+
+  if (rank == sender) data = 500;
+
+  send_data_linear(&data, 1, MPI_INT, sender, receiver, 0, MPI_COMM_WORLD);
+
+  if (rank == receiver) {
+    ASSERT_EQ(500, data);
+  }
 }
 
-TEST(MPI_TESTS, Test_4) {
+TEST(MPI_TESTS, Test_middle_send_inc) {
   int rank = 0;
+  int size_world = 0;
+
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  ASSERT_EQ(1, 1);
+  MPI_Comm_size(MPI_COMM_WORLD, &size_world);
+
+  const int sender = size_world / 2;
+  const int receiver = size_world - 1;
+  int data = 0;
+
+  if (rank == sender) data = 500;
+
+  send_data_linear(&data, 1, MPI_INT, sender, receiver, 0, MPI_COMM_WORLD);
+
+  if (rank == receiver) {
+    ASSERT_EQ(500, data);
+  }
 }
 
-TEST(MPI_TESTS, Test_5) {
+TEST(MPI_TESTS, Test_middle_send_dec) {
   int rank = 0;
+  int size_world = 0;
+
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  ASSERT_EQ(1, 1);
+  MPI_Comm_size(MPI_COMM_WORLD, &size_world);
+
+  const int sender = size_world / 2;
+  const int receiver = 0;
+  int data = 0;
+
+  if (rank == sender) data = 500;
+
+  send_data_linear(&data, 1, MPI_INT, sender, receiver, 0, MPI_COMM_WORLD);
+
+  if (rank == receiver) {
+    ASSERT_EQ(500, data);
+  }
 }
 
 int main(int argc, char** argv) {
   int result_code = 0;
-
-  std::cout << "RESULT: " << sum(10, 20) << '\n';
 
   ::testing::InitGoogleTest(&argc, argv);
   ::testing::TestEventListeners& listeners =
